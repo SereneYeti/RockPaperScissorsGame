9d99276506c865035fa26b9f90f5433e4457e887
James D Vieyra <james@vieyra.co.za>
2022-05-24 17:47:24 +0200

    Basic Setup & housekeeping

--- RockPaperScissorsGame/RPS_Play_ViewController.swift
+++ RockPaperScissorsGame/RPS_Play_ViewController.swift
@@ -14,15 +14,69 @@ class RPS_Play_ViewController: UIViewController {
     }
     
     var chosenAI:ChosenAI = .RandomAI;
+    
+    enum ThrowChoices {
+    case Rock, Paper, Scissors
+    }
+    var playerThrowChoice:ThrowChoices?
+    var prevPlayerThrowChoice:ThrowChoices?
+    var iPlayerWins:Int = 0;
+    var iComputerWins:Int = 0;
+    
+    let sRock = "🪨";
+    let sPaper = "📜";
+    let sScissors = "✂️"
     var textColour:UIColor = UIColor.black;
+    
     @IBOutlet weak var lblChosen_AI: UILabel!
+    @IBOutlet weak var lbl_AI_Choice: UILabel!
+    @IBOutlet weak var lblGameTally: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
         
         _PlayerAIChoice();
+        
+        BasicUISetup();
 
-        // Do any additional setup after loading the view.
+        iPlayerWins = 0;
+        iComputerWins = 0;
+    }
+    @IBOutlet weak var btnChooseRock_Outlet: UIButton!
+    @IBAction func btnChooseRock(_ sender: Any) {
+        playerThrowChoice = .Rock;
+        btnChooseRock_Outlet.backgroundColor = .systemGray;
+        btnChooseScisors_Outlet.backgroundColor = .clear;
+        btnChoosePaper_Outlet.backgroundColor = .clear;
+        print("Rock")
+    }
+    @IBOutlet weak var btnChoosePaper_Outlet: UIButton!
+    @IBAction func btnChoosePaper(_ sender: Any) {
+        playerThrowChoice = .Paper
+        btnChooseScisors_Outlet.backgroundColor = .clear;
+        btnChoosePaper_Outlet.backgroundColor = .systemGray;
+        btnChooseRock_Outlet.backgroundColor = .clear;
+        print("Paper")
+    }
+    @IBOutlet weak var btnChooseScisors_Outlet: UIButton!
+    @IBAction func btnChooseScissors(_ sender: Any) {
+        playerThrowChoice = .Scissors
+        btnChooseScisors_Outlet.backgroundColor = .systemGray;
+        btnChoosePaper_Outlet.backgroundColor = .clear;
+        btnChooseRock_Outlet.backgroundColor = .clear;
+        print("Scissors")
+    }
+    @IBAction func btnMakeThrow(_ sender: Any) {
+        if(playerThrowChoice != nil){
+            prevPlayerThrowChoice = playerThrowChoice;
+            playerThrowChoice = nil;
+        }
+    }
+    @IBAction func btnReturnMainMenu(_ sender: Any) {
+        //Return to Main Menu
+        //print("Returning to Main Menu");
+        self.dismiss(animated: true, completion: nil)
     }
     
     func _PlayerAIChoice() {
@@ -46,13 +100,21 @@ class RPS_Play_ViewController: UIViewController {
        case -1:
            print("no AI Chosen")
            //return to root page;
-           self.navigationController?.popToRootViewController(animated: true);
-           break;
+           self.dismiss(animated: true, completion: nil)
        default:
-           self.navigationController?.popToRootViewController(animated: true);
-           break;
+           self.dismiss(animated: true, completion: nil)
        }
    }
+    
+    func BasicUISetup() -> Void {
+        
+        btnChooseRock_Outlet.titleLabel?.adjustsFontSizeToFitWidth = true;
+        btnChooseScisors_Outlet.titleLabel?.adjustsFontSizeToFitWidth = true;
+        btnChoosePaper_Outlet.titleLabel?.adjustsFontSizeToFitWidth = true;
+        
+        lbl_AI_Choice.textColor = textColour;
+        lblGameTally.text = "Best of 3- Tally:\nPlayer Wins: " + String(iPlayerWins) + "\nAI Wins: " + String(iComputerWins);
+    }
     
     
     
